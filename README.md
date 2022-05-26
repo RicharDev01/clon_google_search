ad8cd4618a4bad381bf52fbabcf90ba84aa81d78
RicharDev01 <ricardopineda587@gmail.com>
2022-05-26 14:19:33 -0600

    [ADD] se agrega la funcionalidad DarkMode

--- README.md
+++ README.md
@@ -4,16 +4,29 @@ En este proyecto realizo la maquetacion de la pagina del buscador de google, apl
 
 Este es una primera version del mismo, ya que pienzo irle agregando modificaciones usando JavaScript, para agregar mayor interactividad al sitio.
 
+> Se agrega funcionalidad del buscador, ya funciona con JavaScript
+
+> Se agrega funcionalidad del Dark Mode
+
+  No se realiza al 100% de detalle ya que mi forma de realizar el Dark Mode fue usando variables CSS (CustomProperties) con lo cual solo modifique las variables, sin tocar nada mas que en este caso la imagen del logo.
+
 # Forma de ver/usar
 
 1. Solo debe de descargar el archivo .zip 
 2. descomprimirlo busque el archivo html `index.html`.
 3. Doble click en `index.html`.
 4. se desplegara la pagina, clon del buscador de google
+5. Escriba en el buscador de google lo que desea
+6. De enter o presione el boton 'Buscar con google'
+7. En la seccion del footer puedes clickear en el enlace 'Preferencia' y cambiara al DarkMode
 
 ![](./image/clon-google.png)
+
+![](./image/clon-google-dark.png)
+
 
 # Tecnologias usadas
 
 - HTML
 - CSS
+- javaScript
--- js/main.js
+++ js/main.js
@@ -1,34 +1,85 @@
-const searchInput = document.getElementById('search-input');
-const btnSearch = document.getElementById('btn-search');
+// VARIABLE GLOBAL PARA USAR 'getElementById' DE FORMA ABREVIADA
+const id = document.getElementById.bind(document);
 
-function mostrarBusqueda(evento){
+/**
+ * DESARROLLO DE LA FUNCIONANLIDAD DE BUSQUEDA DEL CLON DE GOOGLE
+ */
+const searchInput = id("search-input");
+const btnSearch = id("btn-search");
 
+function mostrarBusqueda(evento) {
   evento.preventDefault();
 
-  if(evento.keyCode === 13 && searchInput.value.trim().length > 0){
+  if (evento.keyCode === 13 && searchInput.value.trim().length > 0) {
+    let busqueda = searchInput.value;
+    let url = `https://www.google.com/search?q=${busqueda}`;
 
-    let busqueda = searchInput.value
-    let url = `https://www.google.com/search?q=${busqueda}`
-
-    window.open(url, '_blank');
+    open(url, "_blank");
   }
-
 }
 
-function mostrarBusquedaClick(evento){
-
+function mostrarBusquedaClick(evento) {
   evento.preventDefault();
 
-  if(searchInput.value.trim().length > 0){
+  if (searchInput.value.trim().length > 0) {
+    let busqueda = searchInput.value;
+    let url = `https://www.google.com/search?q=${busqueda}`;
 
-    let busqueda = searchInput.value
-    let url = `https://www.google.com/search?q=${busqueda}`
-
-    window.open(url, '_blank');
+    open(url, "_blank");
   }
-
 }
 
+searchInput.addEventListener("keyup", mostrarBusqueda);
+btnSearch.addEventListener("click", mostrarBusquedaClick);
 
-searchInput.addEventListener('keyup', mostrarBusqueda)
-btnSearch.addEventListener('click', mostrarBusquedaClick)
+/**
+ * FUNCIONANLIDAD DE CAMBIO DE THEME DARK
+ */
+// let col = getComputedStyle( document.documentElement )
+
+let flat = true;
+id("theme").addEventListener("click", () => {
+
+  if (flat) {
+
+    id("logo-google").src = "../image/googlelogo_light_color_272x92.png";
+
+    document.documentElement.style.setProperty("--background-color", "#202124");
+    document.documentElement.style.setProperty(
+      "--background-color-footer",
+      "#171717"
+    );
+    document.documentElement.style.setProperty("--color-dark", "#FFFFFF");
+    document.documentElement.style.setProperty("--color-light", "#303134");
+    document.documentElement.style.setProperty("--color-icon", "#aaaaaa");
+    document.documentElement.style.setProperty(
+      "--color-text-content",
+      "#FFFFFF"
+    );
+    document.documentElement.style.setProperty("--color-border", "#303134");
+    flat = false
+
+  } else {
+
+
+    id("logo-google").src = "../image/googlelogo_color_272x92dp.png";
+
+    document.documentElement.style.setProperty("--background-color", "#FFFFFF");
+    document.documentElement.style.setProperty(
+      "--background-color-footer",
+      "#FFFFFF"
+    );
+    document.documentElement.style.setProperty("--color-dark", "#000000de");
+    document.documentElement.style.setProperty("--color-light", "#FFFFFF");
+    document.documentElement.style.setProperty("--color-icon", "#5f6368");
+    document.documentElement.style.setProperty(
+      "--color-text-content",
+      "#3c4043"
+    );
+    document.documentElement.style.setProperty("--color-border", "#dfe1e5");
+    flat = true
+
+  }
+
+
+});
